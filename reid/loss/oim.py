7ef54c90769ec12eb3b5e34ee0dc1a155a8b1c29
Cysu <st.cysu@gmail.com>
2017-02-26 23:58:11 +0800

    Add OIM loss

---
+++ reid/loss/oim.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, autograd
+
+
+class OIM(autograd.Function):
+    def __init__(self, lut):
+        super(OIM, self).__init__()
+        self.lut = lut
+
+    def forward(self, inputs, targets):
+        self.save_for_backward(inputs, targets)
+        outputs = inputs.mm(self.lut.t())
+        return outputs
+
+    def backward(self, grad_outputs):
+        inputs, targets = self.saved_tensors
+        grad_inputs = None
+        if self.needs_input_grad[0]:
+            grad_inputs = grad_outputs.mm(self.lut)
+        for x, y in zip(inputs, targets):
+            self.lut[y] = 0.5 * (self.lut[y] + x)
+        return grad_inputs, None
+
+
+def oim(inputs, targets, lut):
+    return OIM(lut)(inputs, targets)
+
+
+class OIMLoss(nn.Module):
+    def __init__(self, num_features, num_classes, scalar=1.0,
+                 weight=None, size_average=True):
+        super(OIMLoss, self).__init__()
+        self.num_features = num_features
+        self.num_classes = num_classes
+        self.scalar = scalar
+        self.weight = weight
+        self.size_average = size_average
+
+        self.register_buffer('lut', torch.zeros(num_classes, num_features))
+
+    def forward(self, inputs, targets):
+        inputs = oim(inputs, targets, self.lut)
+        inputs *= self.scalar
+        return F.cross_entropy(inputs, targets, weight=self.weight,
+                               size_average=self.size_average)
---
+++ test/loss/test_OIMLoss.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+
+class TestOIMLoss(TestCase):
+    def test_forward_backward(self):
+        import torch
+        import torch.nn.functional as F
+        from torch.autograd import Variable
+        from reid.loss.oim import OIMLoss
+        criterion = OIMLoss(3, 3, scalar=1.0, size_average=False)
+        criterion.lut = torch.eye(3)
+        x = Variable(torch.randn(3, 3), requires_grad=True)
+        y = Variable(torch.range(0, 2).long())
+        loss = criterion(x, y)
+        loss.backward()
+        probs = F.softmax(x)
+        grads = probs.data - torch.eye(3)
+        abs_diff = torch.abs(grads - x.grad.data)
+        self.assertEquals(torch.log(probs).diag().sum(), -loss)
+        self.assertTrue(torch.max(abs_diff) < 1e-6)
