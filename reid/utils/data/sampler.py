e4881122e80c3add87c7a2b120edbb343b63024a
Cysu <st.cysu@gmail.com>
2017-03-08 19:53:28 +0800

    Triplet sampler

--- reid/utils/data/sampler.py
+++ reid/utils/data/sampler.py
@@ -69,3 +69,38 @@ class RandomPairSampler(Sampler):
 
     def __len__(self):
         return self.num_samples * (1 + self.neg_pos_ratio)
+
+
+class RandomTripletSampler(Sampler):
+    def __init__(self, data_source):
+        super(RandomTripletSampler, self).__init__(data_source)
+        self.data_source = data_source
+        self.num_samples = len(data_source)
+        # Sort by pid
+        indices = np.argsort(np.asarray(data_source)[:, 1])
+        self.index_map = dict(zip(np.arange(self.num_samples), indices))
+        # Get the range of indices for each pid
+        self.index_range = defaultdict(lambda: [self.num_samples, -1])
+        for i, j in enumerate(indices):
+            _, pid, _ = data_source[j]
+            self.index_range[pid][0] = min(self.index_range[pid][0], i)
+            self.index_range[pid][1] = max(self.index_range[pid][1], i)
+
+    def __iter__(self):
+        indices = np.random.permutation(self.num_samples)
+        for i in indices:
+            # anchor sample
+            anchor_index = self.index_map[i]
+            _, pid, _ = self.data_source[anchor_index]
+            # positive sample
+            start, end = self.index_range[pid]
+            pos_index = _choose_from(start, end, excluding=(i, i))[0]
+            pos_index = self.index_map[pos_index]
+            # negative samples
+            neg_index = _choose_from(0, self.num_samples - 1,
+                                     excluding=(start, end))[0]
+            neg_index = self.index_map[neg_index]
+            yield anchor_index, pos_index, neg_index
+
+    def __len__(self):
+        return self.num_samples
