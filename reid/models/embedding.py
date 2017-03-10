7804fa9eef29e61058706170628f4536337aec82
Cysu <st.cysu@gmail.com>
2017-03-10 12:59:10 +0800

    Add Kron embedding

--- reid/models/embedding.py
+++ reid/models/embedding.py
@@ -1,4 +1,7 @@
+import math
 from torch import nn
+
+from .kron import KronMatching
 
 
 class EltwiseSubEmbed(nn.Module):
@@ -33,3 +36,49 @@ class EltwiseSubEmbed(nn.Module):
         else:
             x = x.sum(1)
         return x
+
+
+class KronEmbed(nn.Module):
+    def __init__(self, height, width, out_channels, num_classes):
+        super(KronEmbed, self).__init__()
+        self.kron = KronMatching()
+        in_channels = (2*height-1) * (2*width-1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3,
+                               padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU()
+        self.pool = nn.AvgPool2d((height, width))
+        self.fc = nn.Linear(out_channels, num_classes)
+
+        self.reset_params()
+
+    def forward(self, x1, x2):
+        x = self.kron(x1, x2)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+    def reset_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0, 0.001)
+                if m.bias is not None:
+                    m.bias.data.zero_()
