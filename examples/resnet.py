926ce4cd2df1d9c067501ef2e88fcc98af81a0c5
Cysu <st.cysu@gmail.com>
2017-03-21 16:32:31 +0800

    Include duke dataset in all the examples

--- examples/resnet.py
+++ examples/resnet.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="ID Training ResNet Model")
     # data
     parser.add_argument('-d', '--dataset', type=str, default='cuhk03',
-                        choices=['cuhk03', 'market1501', 'viper'])
+                        choices=['cuhk03', 'market1501', 'viper', 'duke'])
     parser.add_argument('-b', '--batch-size', type=int, default=64)
     parser.add_argument('-j', '--workers', type=int, default=2)
     parser.add_argument('--split', type=int, default=0)
