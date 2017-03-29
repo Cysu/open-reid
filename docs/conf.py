20fa9a28c90bd455d2cd5e766559556840998f6f
Cysu <st.cysu@gmail.com>
2017-03-29 23:10:18 +0800

    [Docs] Strip cloud theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-import cloud_sptheme
 
 # -- General configuration ------------------------------------------------
 
