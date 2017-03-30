f83ac13c06baa87686329fe39c455d8ff86f94fc
Cysu <st.cysu@gmail.com>
2017-03-31 00:06:39 +0800

    [Docs] No import

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,6 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
-import reid
 
 # -- General configuration ------------------------------------------------
 
