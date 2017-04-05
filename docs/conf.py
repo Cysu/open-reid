0e70dc5c9ee3bcd31a96b71f2a4e001ee6df3b3e
Cysu <st.cysu@gmail.com>
2017-04-05 22:26:18 +0800

    Add comment for get_dataset

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,6 @@ extensions = [
     'sphinx.ext.githubpages',
 ]
 
-napoleon_use_ivar = True
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -182,53 +180,3 @@ intersphinx_mapping = {
     'numpy': ('http://docs.scipy.org/doc/numpy/', None),
     'pytorch': ('http://pytorch.org/docs/', None),
 }
-
-# -- A patch that prevents Sphinx from cross-referencing ivar tags -------
-# See http://stackoverflow.com/a/41184353/3343043
-
-from docutils import nodes
-from sphinx.util.docfields import TypedField
-from sphinx import addnodes
-
-
-def patched_make_field(self, types, domain, items):
-    # type: (List, unicode, Tuple) -> nodes.field
-    def handle_item(fieldarg, content):
-        par = nodes.paragraph()
-        par += addnodes.literal_strong('', fieldarg)  # Patch: this line added
-        # par.extend(self.make_xrefs(self.rolename, domain, fieldarg,
-        #                           addnodes.literal_strong))
-        if fieldarg in types:
-            par += nodes.Text(' (')
-            # NOTE: using .pop() here to prevent a single type node to be
-            # inserted twice into the doctree, which leads to
-            # inconsistencies later when references are resolved
-            fieldtype = types.pop(fieldarg)
-            if len(fieldtype) == 1 and isinstance(fieldtype[0], nodes.Text):
-                typename = u''.join(n.astext() for n in fieldtype)
-                typename = typename.replace('int', 'python:int')
-                typename = typename.replace('long', 'python:long')
-                typename = typename.replace('float', 'python:float')
-                typename = typename.replace('type', 'python:type')
-                par.extend(self.make_xrefs(self.typerolename, domain, typename,
-                                           addnodes.literal_emphasis))
-            else:
-                par += fieldtype
-            par += nodes.Text(')')
-        par += nodes.Text(' -- ')
-        par += content
-        return par
-
-    fieldname = nodes.field_name('', self.label)
-    if len(items) == 1 and self.can_collapse:
-        fieldarg, content = items[0]
-        bodynode = handle_item(fieldarg, content)
-    else:
-        bodynode = self.list_type()
-        for fieldarg, content in items:
-            bodynode += nodes.list_item('', handle_item(fieldarg, content))
-    fieldbody = nodes.field_body('', bodynode)
-    return nodes.field('', fieldname, fieldbody)
-
-
-TypedField.make_field = patched_make_field
--- reid/datasets/__init__.py
+++ reid/datasets/__init__.py
@@ -16,6 +16,25 @@ __factory = {
 
 
 def get_dataset(name, root, *args, **kwargs):
+    """
+    Create a dataset instance.
+
+    Parameters
+    ----------
+    name : str
+        The dataset name. Can be one of 'viper', 'cuhk01', 'cuhk03',
+        'market1501', and 'duke'.
+    root : str
+        The path to the dataset directory.
+    split_id : int, optional
+        The index of data split. Default: 0
+    num_val : int or float, optional
+        When int, it means the number of validation identities. When float,
+        it means the proportion of validation to all the trainval.
+        Default: 100 for 'cuhk01' and 'cuhk03', 0.3 for the others
+    download : bool, optional
+        If True, will download the dataset. Default: False
+    """
     if name not in __factory:
         raise KeyError("Unknown dataset:", name)
     return __factory[name](root, *args, **kwargs)
